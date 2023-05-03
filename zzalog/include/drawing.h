21c5d3a998036b2bbfd9d3f87ec322e739969326
Philip Rose <pvrose@btinternet.com>
2023-05-03 14:49:40 +0100

    ZZALOG: 3.4.10 Change: Use red for exit/quit buttons and claret for delete.

--- zzalog/include/drawing.h
+++ zzalog/include/drawing.h
@@ -133,10 +133,11 @@ const int ROW_HEIGHT = DEFAULT_SIZE + 4;
 	const unsigned int WIDTH = 1000;
 	const unsigned int HEIGHT = 650;
 
-	const Fl_Color COLOUR_ORANGE = 93; /* R=4/4, B=0/4, G=5/7 */
-	const Fl_Color COLOUR_APPLE = 87;  /* R=3/4, B=0/4, G=7/7 */
-	const Fl_Color COLOUR_PINK = 170;  /* R=4/4, B=2/4, G=2/7 */
-	const Fl_Color COLOUR_MAUVE = 212; /* R-4/4, B=3/4, G=4/7 */
-	const Fl_Color COLOUR_NAVY = 136;  /* R=0/4, B=2/4, G=0/7 */
+	const Fl_Color COLOUR_ORANGE = 93;       /* R=4/4, B=0/4, G=5/7 */
+	const Fl_Color COLOUR_APPLE = 87;        /* R=3/4, B=0/4, G=7/7 */
+	const Fl_Color COLOUR_PINK = 170;        /* R=4/4, B=2/4, G=2/7 */
+	const Fl_Color COLOUR_MAUVE = 212;       /* R-4/4, B=3/4, G=4/7 */
+	const Fl_Color COLOUR_NAVY = 136;        /* R=0/4, B=2/4, G=0/7 */
+	const Fl_Color COLOUR_CLARET = 80;     /* R=3/4, B=0/4, G=0/4 */
 
 #endif
