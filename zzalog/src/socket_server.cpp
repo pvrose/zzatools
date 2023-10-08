2bf104d75d12f21241587c57cd5f7daad28ce43e
Philip Rose <pvrose@btinternet.com>
2023-10-08 16:02:28 +0100

    ZZALOG 3.4.47 Code tidy: Reduce size of separate code for WIN32 and non-WIN32.

--- zzalog/src/socket_server.cpp
+++ zzalog/src/socket_server.cpp
@@ -24,36 +24,41 @@
 
 #include <FL/Fl.H>
 
-#ifdef _WIN32 
+#ifdef _WIN32
 #define LEN_SOCKET_ADDR int
 #else
 #define LEN_SOCKET_ADDR unsigned int
 #endif
 
-extern status* status_;
+extern status *status_;
 
 // Constructor
-socket_server::socket_server(protocol_t protocol, string address, int port_num) :
-	server_(INVALID_SOCKET),
-	client_(INVALID_SOCKET),
-	protocol_(protocol),
-	port_num_(port_num),
-	th_socket_(nullptr)
+socket_server::socket_server(protocol_t protocol, string address, int port_num) : server_(INVALID_SOCKET),
+																				  client_(INVALID_SOCKET),
+																				  protocol_(protocol),
+																				  port_num_(port_num),
+																				  th_socket_(nullptr)
 {
-	if (address.length()) {
+	if (address.length())
+	{
 		address_ = address;
-	} else {
+	}
+	else
+	{
 		address_ = "0.0.0.0";
 	}
 }
 
 // Create and start the server
-void socket_server::run_server() {
+void socket_server::run_server()
+{
 	closing_ = false;
 	// create server
 	int result = create_server();
-	if (result) {
-		switch (protocol_) {
+	if (result)
+	{
+		switch (protocol_)
+		{
 		case UDP:
 			status_->misc_status(ST_ERROR, "SOCKET: Unable to listen for WSJT-X datagrams");
 			break;
@@ -67,29 +72,29 @@ void socket_server::run_server() {
 }
 
 // Destructor
-socket_server::~socket_server() {
-};
+socket_server::~socket_server(){};
 
 // Close the socket and clean up winsock
-void socket_server::close_server() {
+void socket_server::close_server()
+{
 	closing_ = true;
-	if (th_socket_) th_socket_->detach();
+	if (th_socket_)
+		th_socket_->detach();
 	delete th_socket_;
 	Fl::remove_timeout(cb_timer_acc, this);
 	Fl::remove_timeout(cb_timer_rcv, this);
-	if (server_ != INVALID_SOCKET) {
+	if (server_ != INVALID_SOCKET)
+	{
 		SOCKADDR_IN server_addr;
 		LEN_SOCKET_ADDR len_server_addr = sizeof(server_addr);
 		char message[256];
-		getsockname(server_, (SOCKADDR*)&server_addr, &len_server_addr);
-#ifdef _WIN32
+		getsockname(server_, (SOCKADDR *)&server_addr, &len_server_addr);
 		snprintf(message, 256, "SOCKET: Closing socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
 		status_->misc_status(ST_OK, message);
+#ifdef _WIN32
 		closesocket(server_);
 		WSACleanup();
-#else 
-		snprintf(message, 256, "SOCKET: Closing socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
-		status_->misc_status(ST_OK, message);
+#else
 		close(server_);
 #endif
 		server_ = INVALID_SOCKET;
@@ -97,103 +102,36 @@ void socket_server::close_server() {
 }
 
 // Create the server after initialising winsock and opening socket
-int socket_server::create_server() {
-#ifdef _WIN32
+int socket_server::create_server()
+{
 	int result;
 	// Used for status messages
 	char message[256];
+#ifdef _WIN32
 	// Initailise Winsock
 	WSADATA ws_data;
 	result = WSAStartup(MAKEWORD(2, 2), &ws_data);
-	if (result) {
+	if (result)
+	{
 		handle_error("Unable to initialise winsock");
 		return result;
 	}
-	else {
+	else
+	{
 		status_->misc_status(ST_LOG, "SOCKET: Initialised winsock.");
 	}
 	// Check the version of winsock loaded is 2.2
-	if (ws_data.wVersion != MAKEWORD(2, 2)) {
+	if (ws_data.wVersion != MAKEWORD(2, 2))
+	{
 		snprintf(message, 256, "Incompatible version of winsock %d.%d found", LOBYTE(ws_data.wVersion), HIBYTE(ws_data.wVersion));
 		handle_error(message);
 		return 1;
 	}
+#endif
 
 	// Create the socket
-	switch (protocol_) {
-	case UDP:
-		server_ = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
-		break;
-	case HTTP:
-		server_ = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-		break;
-	}
-	if (server_ == INVALID_SOCKET) {
-		handle_error("Unable to create the socket");
-		return 1;
-	}
-	else {
-		switch (protocol_) {
-		case UDP:
-			status_->misc_status(ST_LOG, "SOCKET: Created UDP socket");
-			break;
-		case HTTP:
-			status_->misc_status(ST_LOG, "SOCKET: Created HTTP socket");
-			break;
-		}
-	}
-
-	// Define the port we listen on and addresses we listen for
-	SOCKADDR_IN server_addr;
-	LEN_SOCKET_ADDR len_server_addr = sizeof(server_addr);
-	server_addr.sin_family = AF_INET;
-	server_addr.sin_port = htons(port_num_);
-	server_addr.sin_addr.s_addr = htonl(inet_addr(address_.c_str()));
-	// Associate the socket with this address data
-	result = bind(server_, (SOCKADDR*)&server_addr, len_server_addr);
-	if (result < 0) {
-		handle_error("Unable to bind the socket");
-		return result;
-	}
-	else {
-		getsockname(server_, (SOCKADDR*)&server_addr, &len_server_addr);
-		snprintf(message, 256, "SOCKET: Connected socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
-		status_->misc_status(ST_OK, message);
-	}
-
-	unsigned long nonblocking = 1;
-	if (server_ != INVALID_SOCKET && ioctlsocket(server_, FIONBIO, &nonblocking)) {
-		handle_error("Unable to make socket non-blocking");
-		return 1;
-	}
-
-	// Set socket into listening mode 
-	if (protocol_ == HTTP) {
-		result = listen(server_, SOMAXCONN);
-		if (result < 0) {
-			handle_error("Unable to establich connection");
-			return result;
-		}
-		else {
-			getsockname(server_, (SOCKADDR*)&server_addr, &len_server_addr);
-			snprintf(message, 256, "SOCKET: Listening socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
-			status_->misc_status(ST_OK, message);
-		}
-
-		// Make the client non-blocking as well
-		unsigned long nonblocking = 1;
-		if (client_ != INVALID_SOCKET && ioctlsocket(client_, FIONBIO, &nonblocking)) {
-			handle_error("Unable to make client socket non-blocking");
-			return 1;
-		}
-	}
-	return 0;
-#else //_WIN32
-	int result;
-	// Used for status messages
-	char message[256];
-	// Create the socket
-	switch (protocol_) {
+	switch (protocol_)
+	{
 	case UDP:
 		server_ = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, IPPROTO_UDP);
 		break;
@@ -201,12 +139,15 @@ int socket_server::create_server() {
 		server_ = socket(AF_INET, SOCK_STREAM | SOCK_NONBLOCK, IPPROTO_TCP);
 		break;
 	}
-	if (server_ == INVALID_SOCKET) {
+	if (server_ == INVALID_SOCKET)
+	{
 		handle_error("Unable to create the socket");
 		return 1;
 	}
-	else {
-		switch (protocol_) {
+	else
+	{
+		switch (protocol_)
+		{
 		case UDP:
 			status_->misc_status(ST_LOG, "SOCKET: Created UDP socket");
 			break;
@@ -221,65 +162,88 @@ int socket_server::create_server() {
 	LEN_SOCKET_ADDR len_server_addr = sizeof(server_addr);
 	server_addr.sin_family = AF_INET;
 	server_addr.sin_port = htons(port_num_);
+#ifdef _WIN32
+	server_addr.sin_addr.s_addr = htonl(inet_addr(address_.c_str()));
+#else
 	server_addr.sin_addr.s_addr = inet_addr(address_.c_str());
+#endif
 	int set_option_on = 1;
-	// it is important to do "reuse address" before bind, not after
-	int res = setsockopt(server_, SOL_SOCKET, SO_REUSEADDR, (char*) &set_option_on, 
-		sizeof(set_option_on));	// Associate the socket with this address data
-	result = bind(server_, (SOCKADDR*)&server_addr, len_server_addr);
+	// Allow address to be reused
+	result = setsockopt(server_, SOL_SOCKET, SO_REUSEADDR, (char*)&set_option_on,
+		sizeof(set_option_on));
 	if (result < 0) {
+		handle_error("Unable to set socket reusable");
+		return result;
+	}
+	// Associate the socket with this address data
+	result = bind(server_, (SOCKADDR *)&server_addr, len_server_addr);
+	if (result < 0)
+	{
 		handle_error("Unable to bind the socket");
 		return result;
 	}
-	else {
-		getsockname(server_, (SOCKADDR*)&server_addr, &len_server_addr);
+	else
+	{
+		getsockname(server_, (SOCKADDR *)&server_addr, &len_server_addr);
 		snprintf(message, 256, "SOCKET: Connected socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
 		status_->misc_status(ST_OK, message);
 	}
 
-	//unsigned long nonblocking = 1;
-	//if (server_ != INVALID_SOCKET && ioctlsocket(server_, FIONBIO, &nonblocking)) {
-		//handle_error("Unable to make socket non-blocking");
-		//return 1;
-	//}
+// #ifdef _WIN32
+// 	unsigned long nonblocking = 1;
+// 	if (server_ != INVALID_SOCKET && ioctlsocket(server_, FIONBIO, &nonblocking))
+// 	{
+// 		handle_error("Unable to make socket non-blocking");
+// 		return 1;
+// 	}
+// #endif
 
-	// Set socket into listening mode 
-	if (protocol_ == HTTP) {
+	// Set socket into listening mode
+	if (protocol_ == HTTP)
+	{
 		result = listen(server_, SOMAXCONN);
-		if (result < 0) {
+		if (result < 0)
+		{
 			handle_error("Unable to establich connection");
 			return result;
 		}
-		else {
-			getsockname(server_, (SOCKADDR*)&server_addr, &len_server_addr);
+		else
+		{
+			getsockname(server_, (SOCKADDR *)&server_addr, &len_server_addr);
 			snprintf(message, 256, "SOCKET: Listening socket %s:%d", inet_ntoa(server_addr.sin_addr), htons(server_addr.sin_port));
 			status_->misc_status(ST_OK, message);
 		}
 
-		//// Make the client non-blocking as well
-		//unsigned long nonblocking = 1;
-		//if (client_ != INVALID_SOCKET && ioctlsocket(client_, FIONBIO, &nonblocking)) {
-			//handle_error("Unable to make client socket non-blocking");
-			//return 1;
-		//}
+// #ifdef _WIN32
+// 		// Make the client non-blocking as well
+// 		unsigned long nonblocking = 1;
+// 		if (client_ != INVALID_SOCKET && ioctlsocket(client_, FIONBIO, &nonblocking))
+// 		{
+// 			handle_error("Unable to make client socket non-blocking");
+// 			return 1;
+// 		}
+// #endif
 	}
 	return 0;
-#endif
 }
 
 // Accept any client asking to connect, use non-blocking call to avoid locking out other code
-socket_server::client_status socket_server::accept_client() {
+socket_server::client_status socket_server::accept_client()
+{
 	char message[256];
 	LEN_SOCKET_ADDR len_client_addr = sizeof(client_addr_);
-	client_ = accept(server_, (SOCKADDR*)&client_addr_, &len_client_addr);
-	if (client_ == INVALID_SOCKET) {
+	client_ = accept(server_, (SOCKADDR *)&client_addr_, &len_client_addr);
+	if (client_ == INVALID_SOCKET)
+	{
 #ifdef _WIN32
-		if (WSAGetLastError() == WSAEWOULDBLOCK) {
-			//status_->misc_status(ST_LOG, "SOCKET: No client socket to accept");
-			// Non-blocking accept would have blocked - let other events get handled and try again
+		if (WSAGetLastError() == WSAEWOULDBLOCK)
+		{
+			// status_->misc_status(ST_LOG, "SOCKET: No client socket to accept");
+			//  Non-blocking accept would have blocked - let other events get handled and try again
 			return BLOCK;
 		}
-		else {
+		else
+		{
 			handle_error("Unable to accept connection");
 			return NG;
 		}
@@ -287,14 +251,16 @@ socket_server::client_status socket_server::accept_client() {
 		return BLOCK;
 #endif
 	}
-	else {
+	else
+	{
 		return OK;
 	}
 }
 const int MAX_SOCKET = 10240;
 
-int socket_server::rcv_packet() {
-	char* buffer = new char[MAX_SOCKET];
+int socket_server::rcv_packet()
+{
+	char *buffer = new char[MAX_SOCKET];
 	int buffer_len = MAX_SOCKET;
 	int bytes_rcvd = 0;
 	// Generate a set of socket descriptors for use in select()
@@ -307,14 +273,17 @@ int socket_server::rcv_packet() {
 	FD_SET(server_, &set_sockets);
 
 	// Now wait until we see a client
-	if (protocol_ == HTTP) {
+	if (protocol_ == HTTP)
+	{
 		client_ = INVALID_SOCKET;
 		client_status result = BLOCK;
-		while (client_ == INVALID_SOCKET && result == BLOCK) {
+		while (client_ == INVALID_SOCKET && result == BLOCK)
+		{
 			result = accept_client();
 			this_thread::yield();
 		}
-		if (result == NG) {
+		if (result == NG)
+		{
 			return 1;
 		}
 	}
@@ -325,17 +294,20 @@ int socket_server::rcv_packet() {
 	string function = "";
 	int pos_payload = 0;
 	LEN_SOCKET_ADDR len_client_addr = sizeof(client_addr_);
-	do {
+	do
+	{
 		// Keep processing packets while they keep coming
-		switch (protocol_) {
+		switch (protocol_)
+		{
 		case UDP:
-			bytes_rcvd = recvfrom(server_, buffer, buffer_len, 0, (SOCKADDR*)&client_addr_, &len_client_addr);
+			bytes_rcvd = recvfrom(server_, buffer, buffer_len, 0, (SOCKADDR *)&client_addr_, &len_client_addr);
 			break;
 		case HTTP:
 			bytes_rcvd = recv(client_, buffer, buffer_len, 0);
 			break;
 		}
-		if (bytes_rcvd > 0) {
+		if (bytes_rcvd > 0)
+		{
 			dump(string(buffer, buffer_len));
 			mu_packet_.lock();
 			string s = string(buffer, bytes_rcvd);
@@ -344,21 +316,24 @@ int socket_server::rcv_packet() {
 			Fl::awake(cb_th_packet, this);
 		}
 #ifdef _WIN32
-		else if (WSAGetLastError() == WSAEWOULDBLOCK) {
+		else if (WSAGetLastError() == WSAEWOULDBLOCK)
+		{
 			// Try again immediately after letting FLTK in
 			this_thread::yield();
 		}
-		else if (WSAGetLastError() == WSAENOTSOCK && closing_
-		) {
+		else if (WSAGetLastError() == WSAENOTSOCK && closing_)
+		{
 			// We can get here through a race between closing and turning the timers off
 			delete[] buffer;
 		}
-		else {
+		else
+		{
 			handle_error("Unable to read from client");
 			delete[] buffer;
 		}
 #else
-		else {
+		else
+		{
 			// Try again after checking for any fltk events
 			this_thread::sleep_for(chrono::milliseconds(1000));
 		}
@@ -370,28 +345,32 @@ int socket_server::rcv_packet() {
 	return 0;
 }
 
-int socket_server::send_response(istream& response) {
+int socket_server::send_response(istream &response)
+{
 	// calculate the data size
 	streampos startpos = response.tellg();
 	response.seekg(0, ios::end);
 	streampos endpos = response.tellg();
 	int resp_size = (int)endpos - (int)startpos;
 	response.seekg(0, ios::beg);
-	// 
-	char* buffer = new char[resp_size + 1];
+	//
+	char *buffer = new char[resp_size + 1];
 	memset(buffer, '\0', resp_size + 1);
 	response.read(buffer, resp_size);
 	dump(string(buffer));
 
 	// Send the response packet
 	int result;
-	if (protocol_ == UDP) {
-		result = sendto(server_, buffer, resp_size, 0, (SOCKADDR*)&client_addr_, sizeof(client_addr_));
+	if (protocol_ == UDP)
+	{
+		result = sendto(server_, buffer, resp_size, 0, (SOCKADDR *)&client_addr_, sizeof(client_addr_));
 	}
-	else {
+	else
+	{
 		result = send(client_, buffer, resp_size, 0);
 	}
-	if (result < 0) {
+	if (result < 0)
+	{
 		handle_error("Unable to send to");
 		return result;
 	}
@@ -399,33 +378,39 @@ int socket_server::send_response(istream& response) {
 }
 
 // Call back to restart attempt to restart datagram
-void socket_server::cb_timer_rcv(void* v) {
-	socket_server* that = (socket_server*)v;
+void socket_server::cb_timer_rcv(void *v)
+{
+	socket_server *that = (socket_server *)v;
 	that->rcv_packet();
 }
 
 // Callback to restart attempt to accept client
-void socket_server::cb_timer_acc(void* v) {
-	socket_server* that = (socket_server*)v;
+void socket_server::cb_timer_acc(void *v)
+{
+	socket_server *that = (socket_server *)v;
 	that->accept_client();
 }
 
 // Has a server
-bool socket_server::has_server() {
+bool socket_server::has_server()
+{
 	return (server_ != INVALID_SOCKET);
 }
 
 // Handle error - display error message
-void socket_server::handle_error(const char* phase) {
+void socket_server::handle_error(const char *phase)
+{
 	char message[1028];
 #ifdef _WIN32
 	DWORD error_code = WSAGetLastError();
-	if (error_code) {
+	if (error_code)
+	{
 		char error_msg[1028];
 		FormatMessage(FORMAT_MESSAGE_FROM_SYSTEM, nullptr, error_code, 0, error_msg, sizeof(error_msg), nullptr);
 		snprintf(message, 1028, "SOCKET: %s: %s", phase, error_msg);
 	}
-	else {
+	else
+	{
 		snprintf(message, 1028, "SOCKET: %s", phase);
 	}
 #else
@@ -436,20 +421,25 @@ void socket_server::handle_error(const char* phase) {
 }
 
 // Set handlers
-void socket_server::callback(int(*request)(stringstream&)) {
+void socket_server::callback(int (*request)(stringstream &))
+{
 	do_request = request;
 }
 
 // Diagnostic print
-void socket_server::dump(string data) {
+void socket_server::dump(string data)
+{
 	string escaped = "";
 	bool newline = false;
 	// For every data byte
-	for (auto it = data.begin(); it != data.end(); it++) {
+	for (auto it = data.begin(); it != data.end(); it++)
+	{
 
-		if (*it >= 7 && *it <= 13) {
-			// Convert control characters to escaped 
-			switch (*it) {
+		if (*it >= 7 && *it <= 13)
+		{
+			// Convert control characters to escaped
+			switch (*it)
+			{
 			case '\a':
 				escaped += "\\a";
 				break;
@@ -477,17 +467,21 @@ void socket_server::dump(string data) {
 				break;
 			}
 		}
-		else if (*it < 32 || *it > 0x7F) {
+		else if (*it < 32 || *it > 0x7F)
+		{
 			// Convert other non-printable or non-ASCII characters to hex escaped form
-			if (newline) {
+			if (newline)
+			{
 				escaped += '\n';
 				newline = false;
 			}
 			char hex[10];
 			snprintf(hex, 10, "\\x%02x", *it);
 		}
-		else if (*it == '\\') {
-			if (newline) {
+		else if (*it == '\\')
+		{
+			if (newline)
+			{
 				escaped += '\n';
 				newline = false;
 			}
@@ -496,7 +490,8 @@ void socket_server::dump(string data) {
 		}
 		else
 		{
-			if (newline) {
+			if (newline)
+			{
 				escaped += '\n';
 				newline = false;
 			}
@@ -506,11 +501,13 @@ void socket_server::dump(string data) {
 }
 
 // main thread side handle packet
-void socket_server::cb_th_packet(void* v) {
-	socket_server* that = (socket_server*)v;
+void socket_server::cb_th_packet(void *v)
+{
+	socket_server *that = (socket_server *)v;
 	// Lock queue while empty it
 	that->mu_packet_.lock();
-	while (!that->q_packet_.empty()) {
+	while (!that->q_packet_.empty())
+	{
 		string s = that->q_packet_.front();
 		stringstream ss;
 		ss.clear();
@@ -525,6 +522,7 @@ void socket_server::cb_th_packet(void* v) {
 }
 
 // Thread runner
-void socket_server::thread_run(socket_server* that) {
+void socket_server::thread_run(socket_server *that)
+{
 	that->rcv_packet();
 }
