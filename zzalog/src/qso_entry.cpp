d0ddc5ab826ddf800258dd076467836a23b16daa
Philip Rose <pvrose@btinternet.com>
2024-01-31 21:29:26 +0000

    ZZALOG: Unnecessary invalidation of QSL status.

--- zzalog/src/qso_entry.cpp
+++ zzalog/src/qso_entry.cpp
@@ -725,8 +725,11 @@ void qso_entry::cb_ip_field(Fl_Widget* w, void* v) {
 		// Remove any dependent fields that may be left over from previous edits
 		that->qso_->unparse();
 	}
-	// QSO has changed, change QSL servers' status
-	that->qso_->invalidate_qsl_status();
+	// QSO has changed, change QSL servers' status 
+	if (field == "FREQ" || field == "BAND" || field == "MODE" || field == "CALL" ||
+		field == "QSO_DATE" || field == "TIME_ON") {
+		that->qso_->invalidate_qsl_status();
+	}
 	// Update other views if editing or logging
 	switch (that->qso_data_->logging_state()) {
 	case qso_data::QSO_INACTIVE:
