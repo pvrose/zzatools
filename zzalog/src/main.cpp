8bb52ab3a752bd1014f0bc227d5413e4738f664c
Philip Rose <pvrose@btinternet.com>
2024-11-22 21:06:22 +0000

    ZZALOG 3.4.97

--- zzalog/src/main.cpp
+++ zzalog/src/main.cpp
@@ -73,7 +73,7 @@ using namespace std;
 string COPYRIGHT = "\302\251 Philip Rose GM3ZZA 2018. All rights reserved.\nPrefix data courtesy of clublog.org";
 string PROGRAM_ID = "ZZALOG";
 string PROG_ID = "ZLG";
-string PROGRAM_VERSION = "3.4.96";
+string PROGRAM_VERSION = "3.4.97";
 string VENDOR = "GM3ZZA";
 extern string TIMESTAMP;
 
--- zzalog/src/timestamp.cpp
+++ zzalog/src/timestamp.cpp
@@ -1,4 +1,4 @@
 #include <string>
 std::string TIMESTAMP = 
-"20241122__143854Z"
+"20241122__210434Z"
 ;
