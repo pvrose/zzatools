7a55b43e313ad9a1e28ba2e322730d5787a672f8
pvrose <gm3zza@btinternet.com>
2025-09-12 21:08:25 +0100

    ZZALOG: In rpc_data_item replace int objevt with int32_t. (todo code review_

--- zzalog/include/rpc_data_item.h
+++ zzalog/include/rpc_data_item.h
@@ -48,8 +48,6 @@ using namespace std;
 	};
 
 	//! This class describes an Remote Procedure Call data item 
-	
-	//! \todo This is not portable to a system with 16-bit integers .
 	class rpc_data_item
 	{
 	public:
@@ -70,13 +68,13 @@ using namespace std;
 		//! Returns the data type of this data item
 		rpc_data_t type();
 		// Get the data as specific type - returms true if is the correct type
-		bool get(int& i);            //!< Receives item as a 32-bit integer, returns false if not integer
+		bool get(int32_t& i);        //!< Receives item as a 32-bit integer, returns false if not integer
 		bool get(string& s);         //!< Receives item as a string, returns false if not a string
 		bool get(double& d);         //!< Receives item as a double, returns false if not floating point
 		bool get(rpc_array*& ap);    //!< Receives iten as an array, returns false if not an array.
 		bool get(rpc_struct*& mp);   //!< Receives item as a structure, returns false if not a structure.
 		// Returns the data as specific type
-		int get_int();               //!< Returns item as a 32-bit integer
+		int32_t get_int();           //!< Returns item as a 32-bit integer
 		string get_string();         //!< Returns item as a string
 		double get_double();         //!< Returns item as a double-precision value
 		rpc_array* get_array();      //!< Returns item as an array
@@ -85,7 +83,7 @@ using namespace std;
 		//! Convert the item to a textual format
 		string print_item();
 		//! Set the data as int or bool
-		void set(int i, rpc_data_t Type);
+		void set(int32_t i, rpc_data_t Type);
 		//! Set the data as a string or byte encoded string or a date/time
 		void set(string s, rpc_data_t Type);
 		void set(double d);          //!< Set the item as a double-precision value.
@@ -96,7 +94,7 @@ using namespace std;
 		//! The type of data
 		rpc_data_t type_;
 		//! Its representation as a 32-bit integer
-		int i_;
+		int32_t i_;
 		//! Its representation as a string
 		string s_;
 		//! Its representation as a double-precison value
