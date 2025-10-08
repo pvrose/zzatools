34a628ea112271107022366550f4841d1c6eba9f
pvrose <gm3zza@btinternet.com>
2025-10-08 10:10:25 +0100

    ZZALOG: Remove unused filenames from files.h.

--- zzalog/include/files.h
+++ zzalog/include/files.h
@@ -1,26 +1,13 @@
 /*! Types and constants used by file related classes
-    \todo This file has fallen out of use. Either remove it or move all reference data
-	file names here.
 */
 #ifndef __FILES__
 #define __FILES__
 
-#include <string>
+//! Results from loading a file
+enum load_result_t {
+	LR_GOOD,     //!< loaded OK.
+	LR_BAD,      //!< failed to complete loading
+	LR_EOF,      //!< EOF read
+};
 
-
-
-
-
-	//! Results from loading a file
-	enum load_result_t {
-		LR_GOOD,     //!< loaded OK.
-		LR_BAD,      //!< failed to complete loading
-		LR_EOF,      //!< EOF read
-	};
-
-	//! Default prefix database
-	const std::string PREFIX_FILE = "/Prefix.lst";
-
-	//! Default specification database
-	const std::string ADIF_FILE = "all.xml";
 #endif
